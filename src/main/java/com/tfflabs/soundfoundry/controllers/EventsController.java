e83fa67d309ad692a6b4a2c5952c90070b606fed
Adrian Moya <adrianmoya@gmail.com>
2017-09-14 15:29:03 -0500

    Various fixes from comments

--- src/main/java/com/tfflabs/soundfoundry/controllers/EventsController.java
+++ src/main/java/com/tfflabs/soundfoundry/controllers/EventsController.java
@@ -2,31 +2,23 @@ package com.tfflabs.soundfoundry.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import com.tfflabs.soundfoundry.dto.Event;
-import com.tfflabs.soundfoundry.services.RoomService;
 
 @Controller
 public class EventsController {
 
-	@Autowired
-	private RoomService roomService;
-	
 	private Logger log = LoggerFactory.getLogger(EventsController.class);
 	
 	@MessageMapping("/events")
 	@SendTo("/topic/events")
 	public Event processEvent(Event event){
-		log.info("Entering this shit");
-		log.debug("Event: {}", event);
+
 		switch(event.getType()){
 		
 		case ADDTRACK:
-			//Track track = new Track();
-			//roomService.addTrack(null, "");
 			log.debug("Evento recibido: {}", event.toJson());
 			break;
 		default:
--- src/main/java/com/tfflabs/soundfoundry/dto/Event.java
+++ src/main/java/com/tfflabs/soundfoundry/dto/Event.java
@@ -5,27 +5,14 @@ import java.util.Map;
 
 import com.google.gson.Gson;
 
+import lombok.Data;
+
+@Data
 public class Event {
 
 	private String user;
 	private EventType type;
-	public EventType getType() {
-		return type;
-	}
-
-	public void setType(EventType type) {
-		this.type = type;
-	}
-
 	private Map<String, String> context = new HashMap<>();
-	
-	public void setContext(String key, String value){
-		context.put(key, value);
-	}
-	
-	public String getContext(String key){
-		return context.get(key);
-	}
 	
 	public Event fromJson(String json){
 		Event event = new Event();
@@ -36,20 +23,4 @@ public class Event {
 		return gson.toJson(this);
 	}
 
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-	public Map<String, String> getContext() {
-		return context;
-	}
-
-	public void setContext(Map<String, String> context) {
-		this.context = context;
-	}
-	
 }
